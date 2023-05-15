558728b78b1247f4e25452a4e71383979715d9b2
AbdulazizRatah <133403930+AbdulazizRatah@users.noreply.github.com>
2023-05-15 16:02:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,28 @@ const BugTracker = () => {
     setNewBug({ name: '', description: '' });
   };
 
+  // Function to handle updating the status and description of a bug
+const updateBugStatusAndDescription = (id, newStatus, newDescription) => {
+  const updatedBugs = bugs.map(bug => {
+    if (bug.id === id) {
+      return { ...bug, status: newStatus, description: newDescription };
+    }
+    return bug;
+  });
+
+
+  // Function to handle updating the description of a bug
+const updateBugDescription = (id, newDescription) => {
+  const updatedBugs = bugs.map(bug => {
+    if (bug.id === id) {
+      return { ...bug, description: newDescription };
+    }
+    return bug;
+  });
+
+  setBugs(updatedBugs);
+};
+
   // Function to handle updating the status of a bug
   const updateBugStatus = (id, newStatus) => {
     const updatedBugs = bugs.map(bug => {
@@ -33,6 +55,11 @@ const BugTracker = () => {
       return bug;
     });
 
+
+  setBugs(updatedBugs);
+};
+
+    
     setBugs(updatedBugs);
   };
 
@@ -41,40 +68,55 @@ const BugTracker = () => {
       <h1>Bug Tracker</h1>
 
       <form onSubmit={addBug}>
-        <input
+        <div><input
           type="text"
           value={newBug.name}
           onChange={e => setNewBug({ ...newBug, name: e.target.value })}
           placeholder="Enter bug name"
-        />
-        <input
+        /></div>
+        <div><input
           type="text"
           value={newBug.description}
           onChange={e => setNewBug({ ...newBug, description: e.target.value })}
           placeholder="Enter bug description"
-        />
+        /></div>
         <button type="submit">Add Bug</button>
       </form>
 
       <ul>
-        {bugs.map(bug => (
-          <li key={bug.id}>
-            <div>{bug.name}</div>
-            <div>{bug.description}</div>
-            <div>Status: {bug.status}</div>
-            <div>Reported at: {bug.timestamp}</div>
-            <button onClick={() => updateBugStatus(bug.id, 'Fixed')}>
-              Mark as Fixed
-            </button>
-            <button onClick={() => updateBugStatus(bug.id, 'In Progress')}>
-              Mark as In Progress
-            </button>
-            <button onClick={() => updateBugStatus(bug.id, 'Open')}>
-              Reopen
-            </button>
-          </li>
-        ))}
-      </ul>
+  {bugs.map(bug => (
+    <li key={bug.id}>
+      <div>{bug.name}</div>
+      <div>{bug.description}</div>
+      <div>Status: {bug.status}</div>
+      <div>Reported at: {bug.timestamp}</div>
+  
+      <button onClick={() => updateBugStatusAndDescription(bug.id, 'Fixed', bug.description)}>
+        Mark as Fixed
+      </button>
+      <button onClick={() => updateBugStatusAndDescription(bug.id, 'In Progress', bug.description)}>
+        Mark as In Progress
+      </button>
+      <button onClick={() => updateBugStatusAndDescription(bug.id, 'Open', bug.description)}>
+        Reopen
+      </button>
+      <div><input
+        type="text"
+      
+        size="30"
+        onChange={e => updateBugStatusAndDescription(bug.id, bug.status, e.target.value)}
+        placeholder="Update bug description"
+      /></div>
+      <form>
+      <button onClick={() => updateBugStatusAndDescription(bug.id, bug.status, bug.description)}>
+        Update Status and Description
+      </button>
+      </form>
+    </li>
+  ))}
+</ul>
+
+
     </div>
   );
 };
