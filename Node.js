a11af620d716b98338cd22083c95883a4987621b
AbdulazizRatah <133403930+AbdulazizRatah@users.noreply.github.com>
2023-05-15 20:30:49 +0300

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,33 +1,103 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+const PORT = 3000;
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
+app.use(bodyParser.json());
 
-// Route definitions
+// Define routes
+
+// Get all bugs
 app.get('/bugs', (req, res) => {
-  // Handle GET request for bug list
-  // Implement your logic to fetch bug data from the appropriate service or database
-  // and send the response back to the client
-  res.json({ message: 'GET /bugs - Fetch bug list' });
+  res.json(bugs);
 });
 
+// Create a new bug
 app.post('/bugs', (req, res) => {
-  // Handle POST request to add a new bug
-  // Implement your logic to create a new bug in the appropriate service or database
-  // using the data from the request body and send the response back to the client
-  res.json({ message: 'POST /bugs - Create a new bug' });
+  const { name, description } = req.body;
+
+  if (!name || !description) {
+    return res.status(400).json({ error: 'Bug name and description are required.' });
+  }
+
+  const bug = {
+    id: Date.now(),
+    name,
+    description,
+    status: 'Open',
+    timestamp: new Date().toLocaleString(),
+  };
+
+  bugs.push(bug);
+  res.status(201).json(bug);
 });
 
+// Update bug status and description
 app.put('/bugs/:id', (req, res) => {
-  // Handle PUT request to update a bug
-  // Implement your logic to update the bug with the given ID in the appropriate service or database
-  // using the data from the request body and send the response back to the client
-  const bugId = req.params.id;
-  res.json({ message: `PUT /bugs/${bugId} - Update bug with ID ${bugId}` });
+  const { id } = req.params;
+  const { status, description } = req.body;
+
+  const bug = bugs.find(bug => bug.id === parseInt(id));
+
+  if (!bug) {
+    return res.status(404).json({ error: 'Bug not found.' });
+  }
+
+  bug.status = status || bug.status;
+  bug.description = description || bug.description;
+
+  res.json(bug);
+});
+
+// Update bug status
+app.patch('/bugs/:id/status', (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const bug = bugs.find(bug => bug.id === parseInt(id));
+
+  if (!bug) {
+    return res.status(404).json({ error: 'Bug not found.' });
+  }
+
+  bug.status = status || bug.status;
+
+  res.json(bug);
+});
+
+// Update bug description
+app.patch('/bugs/:id/description', (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+
+  const bug = bugs.find(bug => bug.id === parseInt(id));
+
+  if (!bug) {
+    return res.status(404).json({ error: 'Bug not found.' });
+  }
+
+  bug.description = description || bug.description;
+
+  res.json(bug);
+});
+
+// Delete a bug
+app.delete('/bugs/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = bugs.findIndex(bug => bug.id === parseInt(id));
+
+  if (index === -1) {
+    return res.status(404).json({ error: 'Bug not found.' });
+  }
+
+  const deletedBug = bugs.splice(index, 1)[0];
+
+  res.json(deletedBug);
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('API Gateway listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
